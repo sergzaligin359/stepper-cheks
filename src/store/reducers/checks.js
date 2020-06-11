9bc837ab3c1eb0a75611b447611e08fceff2b5c8
sergey <siteabouteat@gmail.com>
2020-06-11 14:26:42 +0300

    fix

---
+++ src/store/reducers/checks.js
@@ -0,0 +1,23 @@
+import { FETCH_CHECKS_START, FETCH_CHECKS_SUCCESS, FETCH_CHECKS_FAILURE } from '../types'
+
+const initialState = {}
+
+export const checks = (state = initialState, {type, payload}) => {
+    switch (type) {
+        case FETCH_CHECKS_START:
+            return {
+                ...state
+            }
+        case FETCH_CHECKS_SUCCESS:
+            return {
+                ...state,
+                phones: payload
+            }
+        case FETCH_CHECKS_FAILURE:
+            return {
+                ...state
+            }
+        default:
+            return state
+    }   
+}
