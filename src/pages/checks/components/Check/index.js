b13a409894aa33aefa50b35d5250cbcbe1efff6d
sergey <siteabouteat@gmail.com>
2020-06-12 15:09:17 +0300

    fix

--- src/pages/checks/components/Check/index.js
+++ src/pages/checks/components/Check/index.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
+import Stepper from '../../../../components/Stepper'
+
 export default ({ mode }) => {
     return(
         <div>
-            Check component - mode { mode }
+            <div>
+                Check component - mode { mode }
+            </div>
+            <div>
+                <Stepper />
+            </div>
         </div>
     )
 }
